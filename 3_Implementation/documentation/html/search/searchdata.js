5f9d87825c93001f4ae6fada2401422a0b7efbc8
Bandru Sravan Kumar <101509869+Sravan4803@users.noreply.github.com>
2022-04-22 16:08:38 +0530

    Add files via upload

---
+++ 3_Implementation/documentation/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "ehmv",
+  1: "v",
+  2: "h",
+  3: "m",
+  4: "e"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
